28ab9896914ce6c2ff8cb10613aff76d09d84c04
arwabalawi <arwa62885@gmail.com>
2021-12-29 09:11:47 +0300

    D-2021_12_29 teacher interface

---
+++ Estaed $/Estaed/Model/ChallengDetailsModel.swift
@@ -0,0 +1,19 @@
+//
+//  m.swift
+//  Estaed
+//
+//  Created by arwa balawi on 24/05/1443 AH.
+//
+
+import Foundation
+
+struct ChallengDetails {
+
+var quistiontOfCH : String
+var answersA : String
+var answerB : String
+var answersC : String
+var answersD : String
+var correctAnswer : String
+var solutionMethod : String
+}
