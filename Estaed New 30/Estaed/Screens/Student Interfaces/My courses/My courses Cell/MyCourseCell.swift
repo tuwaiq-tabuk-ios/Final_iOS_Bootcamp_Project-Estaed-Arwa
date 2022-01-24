df68c4b30689066bac8b3d4f577e486e975d119e
arwabalawi <arwa62885@gmail.com>
2022-01-24 18:08:39 +0300

    D-2022-01-24-student interface design

---
+++ Estaed New 30/Estaed/Screens/Student Interfaces/My courses/My courses Cell/MyCourseCell.swift
@@ -0,0 +1,39 @@
+//
+//  MyCourseCell.swift
+//  Estaed
+//
+//  Created by arwa balawi on 06/06/1443 AH.
+//
+
+import UIKit
+
+class MyCourseCell: UITableViewCell {
+  
+  @IBOutlet weak var courseName: UILabel!
+  @IBOutlet weak var courseTimeKind: UILabel!  
+  @IBOutlet weak var LecturBtn: UIButton!
+    @IBOutlet weak var challingBtn: UIButton!
+  
+    var cellDelegete : MyCellDelegate!
+  
+  
+  override func awakeFromNib() {
+      super.awakeFromNib()
+     
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+      super.setSelected(selected, animated: animated)
+
+  }
+  
+  
+  @IBAction func LecturBtn(_ sender: UIButton) {
+    cellDelegete.didPressButton(sender.tag)
+  }
+  
+    @IBAction func challengeBtnTapped(_ sender: UIButton) {
+        cellDelegete.didPressButton(sender.tag)
+    }
+    
+}
